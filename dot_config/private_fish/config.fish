6da8368c0eda96ac7333d7c0eccb2a7c19b15ae2
Kacper Adamczyk <kacper@admck.com>
2025-09-17 12:20:57 +0200

    Ghostty config adjustments

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,6 +1,10 @@
 if status is-interactive
     starship init fish | source
     zoxide init fish | source
+
+    # set ZELLIJ_AUTO_ATTACH true
+    # set ZELLIJ_AUTO_EXIT true
+    # eval (zellij setup --generate-auto-start fish | string collect)
 end
 
 # uv
